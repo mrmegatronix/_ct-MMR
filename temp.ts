2f0b8293249afdcff8a6da26e749ab8c213b0907
mrmegatronix <whitecomet2014@gmail.com>
2026-03-31 00:52:15 +1300

    feat: split title, increase density, format sizes, clean repo

--- temp.ts
+++
@@ -1,8 +0,0 @@
-async function getColors() {
-  const html = await fetch('https://www.coasterstavern.co.nz').then(r => r.text());
-  const styles = html.match(/<style[^>]*>([\s\S]*?)<\/style>/gi);
-  if (styles) {
-    console.log(styles.join('\n').match(/--[a-zA-Z0-9-]+:\s*(#[0-9A-Fa-f]{3,6}|rgba?\([^)]+\))/g));
-  }
-}
-getColors();
